063d22d65b187cf50da7cd206c64bdd4d6c319ac
metru.dev <sam@zanca.dev>
2026-08-09 22:58:13 -0400

    fix: simplified herdr-remote function

--- home/.config/fish/functions/herdr-remote.fish
+++ home/.config/fish/functions/herdr-remote.fish
@@ -4,16 +4,10 @@ function herdr-remote --description 'Attach to a remote Herdr session over SSH'
         return 127
     end
 
-    if test (count $argv) -lt 1
-        printf 'Usage: herdr-remote <ssh-host> [session]\n' >&2
-        return 2
+    set -l host desktop
+    if test (count $argv) -ge 1
+        set host $argv[1]
     end
 
-    set -l host $argv[1]
-    set -l session agents
-    if test (count $argv) -ge 2
-        set session $argv[2]
-    end
-
-    herdr --remote $host --session $session
+    herdr --remote $host
 end
