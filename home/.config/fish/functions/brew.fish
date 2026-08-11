474092e757e39d8c230719b5ebaf8f946eccde39
metru.dev <sam@zanca.dev>
2026-08-11 12:51:14 -0400

    fix: more agressive blocking of brew

--- home/.config/fish/functions/brew.fish
+++ home/.config/fish/functions/brew.fish
@@ -1,5 +1,4 @@
 function brew
     echo "Prefer editing ~/.config/nix-darwin and running make switch." >&2
     echo "Homebrew changes made here will be overwritten on the next rebuild." >&2
-    command brew $argv
 end
